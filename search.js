fadc0ba58db40e086561d743fd1edad73d7f2845
Dewmmi <dewmmisanark@gmail.com>
2025-12-31 18:38:16 +0100

    Update search.js

--- search.js
+++ search.js
@@ -1,58 +1,53 @@
-const API_KEY = "cac778da";
+document.addEventListener("DOMContentLoaded", function () {
 
-var page = 1;
-var searchTerm = "star wars";
+  const API_KEY = "cac778da";
 
-var container = document.getElementById("movies-container");
-var loadMoreButton = document.getElementById("load-more");
-var searchInput = document.getElementById("search-input");
+  var page = 1;
+  var searchTerm = "star wars";
 
-function loadMovies(clear) {
+  var container = document.getElementById("movies-container");
+  var loadMoreButton = document.getElementById("load-more");
+  var searchInput = document.getElementById("search-input");
 
-  if (clear === true) {
-    container.innerHTML = "";
+  function loadMovies(clear) {
+
+    if (clear === true) {
+      container.innerHTML = "";
+    }
+
+    fetch("https://www.omdbapi.com/?apikey=" + API_KEY + "&s=" + searchTerm + "&page=" + page)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+
+        if (!data.Search) return;
+
+        for (var i = 0; i < data.Search.length; i++) {
+
+          var movie = data.Search[i];
+
+          container.innerHTML +=
+            "<div class='movie'>" +
+              "<img src='" + movie.Poster + "' alt='" + movie.Title + "'>" +
+              "<h3>" + movie.Title + "</h3>" +
+              "<a href='movie.html?id=" + movie.imdbID + "'>Details</a>" +
+            "</div>";
+        }
+      });
   }
 
-  fetch("https://www.omdbapi.com/?apikey=" + API_KEY + "&s=" + searchTerm + "&page=" + page)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
+  loadMoreButton.onclick = function () {
+    page++;
+    loadMovies(false);
+  };
 
-      if (data.Search == undefined) {
-        return;
-      }
+  searchInput.oninput = function (event) {
+    searchTerm = event.target.value.trim() || "avengers";
+    page = 1;
+    loadMovies(true);
+  };
 
-      for (var i = 0; i < data.Search.length; i++) {
-
-        var movie = data.Search[i];
-
-        container.innerHTML =
-          container.innerHTML +
-          "<div class='movie'>" +
-            "<img src='" + movie.Poster + "' alt='" + movie.Title + "'>" +
-            "<h3>" + movie.Title + "</h3>" +
-            "<a href='movie.html?id=" + movie.imdbID + "'>Details</a>" +
-          "</div>";
-      }
-    });
-}
-
-loadMoreButton.onclick = function () {
-  page = page + 1;
-  loadMovies(false);
-};
-
-searchInput.oninput = function (event) {
-
-  if (event.target.value.trim() === "") {
-    searchTerm = "avengers";
-  } else {
-    searchTerm = event.target.value;
-  }
-
-  page = 1;
   loadMovies(true);
-};
 
-loadMovies(true);
+});
